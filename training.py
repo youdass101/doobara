24d004360a901db621a099362441da436e502836
Aech <hamzechalhoub@gmail.com>
2021-04-22 00:39:15 +0300

    trainng

--- training.py
+++ training.py
@@ -1,17 +1,51 @@
 
 annual_salary = float(input("Enter your annual salary:"))
-portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
-total_cost = float(input("Enter the cost of your dream home:"))
-semi_annual_raise = float(input("Enter the semi­annual raise, as a decimal:"))
+
+portion_saved = 0
+total_cost = 1000000
+epsilon = 100
+counter = 0
+semi_annual_raise = 0.7
 portion_down_payment = total_cost * 0.25
 current_savings = 0 
 monthly_salary = annual_salary / 12
 months = 0 
 r = 0.04
-while current_savings < portion_down_payment:
-    if months > 0 and months % 6 == 0:
-        monthly_salary += monthly_salary * semi_annual_raise
-    current_savings += (current_savings * r)/12
-    months += 1
-    current_savings += monthly_salary * portion_saved
-print("Number of months:",months)
+low = 0
+high = 10000
+guess = int((high+low)/2)
+
+print("first guess", guess)
+
+while abs(current_savings - portion_down_payment) >= epsilon:
+    current_savings = 0
+    months = 0
+    guess = float(guess / 10000)
+    print(guess)
+    # CALACULATE THE GUESS
+    while months < 36:
+
+        # RAISE CACULATE
+        if months > 0 and months % 6 == 0:
+            monthly_salary += monthly_salary * semi_annual_raise
+        current_savings += (current_savings * r)/12
+        months += 1
+        current_savings += monthly_salary * guess
+        current_savings = int(current_savings)
+        
+    
+    if current_savings < portion_down_payment:
+        low = int(guess * 10000)
+    else:
+        high = int(guess * 10000)
+    guess = int((high + low)/2)
+    counter += 1
+    if guess == 0:
+        break
+    print(current_savings)
+
+
+
+
+
+print("rate is:",guess)
