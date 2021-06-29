e61cbb814700208924cd30e37f069fad0421bf21
youdass101 <hamzechalhoub@gmail.com>
2021-06-30 01:58:17 +0300

    menu

--- doobarashop/static/doobarashop/main.js
+++ doobarashop/static/doobarashop/main.js
@@ -1,24 +1,28 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    var menubtn = document.getElementById('humberger');
+    var menubtn = document.getElementById('btnm');
     var mobilemenu = document.getElementById('navigation-mobile');
 
     // When the user clicks on the button, open the modal
     menubtn.onclick = function() {
-        if (mobilemenu.style.display == 'block') {
-            mobilemenu.style.display = "none";
-        }
-        else {
-            mobilemenu.style.display = 'block';
-        }
-        
+        mobilemenu.classList.toggle('show');
     }
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == mobilemenu) {
-            alert("asd")
-            mobilemenu.style.display = "none";
-        }
-    }
+    // menubtn.onclick = function() {
+    //     if (mobilemenu.style.display == 'block') {
+    //         mobilemenu.style.display = "none";
+    //     }
+    //     else {
+    //         mobilemenu.style.display = 'block';
+    //     }
+        
+    // }
+
+    
+    // // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function(event) {
+    //     if (event.target !== mobilemenu && event.target !== menubtn ) {
+    //         mobilemenu.style.display = "none";
+    //     }
+    // }
 })
