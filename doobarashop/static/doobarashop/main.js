69a09575a6358c2d14f3ad60dcbeac128114fff3
youdass101 <hamzechalhoub@gmail.com>
2021-07-01 01:06:56 +0300

    sticky menu

--- doobarashop/static/doobarashop/main.js
+++ doobarashop/static/doobarashop/main.js
@@ -1,28 +1,41 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+    // Mobile humberger button for menu
     var menubtn = document.getElementById('btnm');
+    // Mobile navigation menu
     var mobilemenu = document.getElementById('navigation-mobile');
+    // Navigation bar 
+    var navbar = document.getElementById('navbar');
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
 
-    // When the user clicks on the button, open the modal
+    // When the user clicks on the button, open the mobile navigation menu
     menubtn.onclick = function() {
         mobilemenu.classList.toggle('show');
     }
 
-    // menubtn.onclick = function() {
-    //     if (mobilemenu.style.display == 'block') {
-    //         mobilemenu.style.display = "none";
-    //     }
-    //     else {
-    //         mobilemenu.style.display = 'block';
-    //     }
-        
-    // }
+    // When the user clicks anywhere outside of the mobile navigation
+    window.onclick = function(event) {
+        if (event.target !== mobilemenu && event.target !== menubtn) {
+            mobilemenu.classList.toggle('show', false);
+        }   
+    }
 
-    
-    // // When the user clicks anywhere outside of the modal, close it
-    // window.onclick = function(event) {
-    //     if (event.target !== mobilemenu && event.target !== menubtn ) {
-    //         mobilemenu.style.display = "none";
-    //     }
+    window.onscroll = function() {myFunction()};
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+        } else {
+        navbar.classList.remove("sticky");
+        }
+    }
+        // window.onscroll = function() {
+    //     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    //         navbar.classList.toggle('stickyNav', true);
+    //       } else {
+    //         navbar.classList.toggle('stickyNav', false);
+    //       }
+
     // }
 })
