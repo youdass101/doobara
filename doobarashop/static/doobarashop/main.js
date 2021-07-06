064fc541f851abdb21e5f02e1e0c83923f5dfd30
youdass101 <hamzechalhoub@gmail.com>
2021-07-07 00:19:56 +0300

    start with slider

--- doobarashop/static/doobarashop/main.js
+++ doobarashop/static/doobarashop/main.js
@@ -30,12 +30,34 @@ document.addEventListener('DOMContentLoaded', function(){
         navbar.classList.remove("sticky");
         }
     }
-        // window.onscroll = function() {
-    //     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    //         navbar.classList.toggle('stickyNav', true);
-    //       } else {
-    //         navbar.classList.toggle('stickyNav', false);
-    //       }
+   
+    // SLIDER ON INDEX PAGE 
+    var slideIndex = 1;
+    showSlides(slideIndex);
 
-    // }
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    }
 })
