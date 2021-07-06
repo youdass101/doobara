c1d2edb470661cb49f9c2fc0323cc7f30e3414ef
youdass101 <hamzechalhoub@gmail.com>
2021-07-07 01:30:13 +0300

    slider work

--- doobarashop/static/doobarashop/main.js
+++ doobarashop/static/doobarashop/main.js
@@ -25,12 +25,12 @@ document.addEventListener('DOMContentLoaded', function(){
     // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
     function myFunction() {
         if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+        navbar.classList.add("sticky");
         } else {
         navbar.classList.remove("sticky");
         }
     }
-   
+
     // SLIDER ON INDEX PAGE 
     var slideIndex = 1;
     showSlides(slideIndex);
@@ -46,18 +46,21 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    }
+   
+    
 })
+
