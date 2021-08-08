4b1d8db6f381e197f4674f6cd4f1f2a5534ce93e
youdass101 <hamzechalhoub@gmail.com>
2021-08-09 02:19:49 +0300

    shop page

--- doobarashop/urls.py
+++ doobarashop/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("shop", views.shop, name="shop")
+
 ]
 
--- doobarashop/views.py
+++ doobarashop/views.py
@@ -1,7 +1,12 @@
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
     return render(request, "doobarashop/index.html")
+
+
+def shop(request):
+    return render(request, "doobarashop/shop.html")
 
 
