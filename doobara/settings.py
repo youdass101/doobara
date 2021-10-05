fa898fca9e27294f6fe578506a75dd796024045d
Aech <hamzechalhoub@gmail.com>
2021-10-05 23:11:38 +0300

    postpone cookie issue

--- doobara/settings.py
+++ doobara/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+# i don't think this works I guess I have to install django-cookie-samesite
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SAMESITE = 'None'
+# SESSION_COOKIE_SAMESITE = 'None'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
