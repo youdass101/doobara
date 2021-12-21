d2c5ac97cf28d11369ea8a11acab997aa0cf2112
Aech <hamzechalhoub@gmail.com>
2021-12-21 19:42:58 +0200

    product modeling

--- doobarashop/models.py
+++ doobarashop/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from datetime import datetime, timedelta
+from django.conf import settings
+from django.db.models.base import Model
 # from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+# PRODUCT is sql table
+# interp.  product table 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    stock = models.BooleanField()
+    featured = models.BooleanField()
+    
