7ccc4e3f760aa63ec8e45b476923c07eef1594f9
youdass101 <hamzechalhoub@gmail.com>
2022-01-08 17:54:03 +0200

    to merge

--- doobarashop/models.py
+++ doobarashop/models.py
@@ -1,16 +1,16 @@
-from django.db import models
+ from django.db import models
 from datetime import datetime, timedelta
 from django.conf import settings
 from django.db.models.base import Model
+from django.db.models.fields.related import ForeignKey
 # from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 
-# PRODUCT is sql table
-# interp.  product table 
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.IntegerField()
-    stock = models.BooleanField()
-    featured = models.BooleanField()
+
+
+
+
+ 
+
     
