14e2dfa1c9e6269b1bd56e9182bafaf5b2e63792
youdass101 <hamzechalhoub@gmail.com>
2022-01-08 19:15:40 +0200

    models database

--- doobarashop/models.py
+++ doobarashop/models.py
@@ -6,7 +6,32 @@ from django.db.models.fields.related import ForeignKey
 # from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+# PRODUCT is sql table
+# interp.  product table 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    short_description = models.TextField()
+    long_description = models.TextField()
+    created_time = models.DateTimeField()
+    video = models.URLField()
+    stock = models.BooleanField()
+    featured = models.BooleanField()
+    variant = models.BooleanField()
+    active = models.BooleanField()
+    discount = models.BooleanField()
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.name} "
 
+class Categories(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    child_node = models.ForeignKey(Categories)
+    right_node = models.ForeignKey(Categories)
+    
+    def __str__(self):
+        return f"{self.name} "
 
 
 
