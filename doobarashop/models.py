8464571e9742c7bab68065bc842d7055fec10dc2
youdass101 <hamzechalhoub@gmail.com>
2022-01-09 02:07:40 +0200

    modeling

--- doobarashop/models.py
+++ doobarashop/models.py
@@ -1,9 +1,20 @@
- from django.db import models
+from django.db import models
 from datetime import datetime, timedelta
 from django.conf import settings
 from django.db.models.base import Model
+from django.db.models.deletion import SET_NULL
 from django.db.models.fields.related import ForeignKey
 # from django.contrib.auth.models import AbstractBaseUser
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    child_node = models.ForeignKey("self",null=True, blank=True, default=None, on_delete=models.SET_NULL)
+    right_node = models.ForeignKey("self",null=True, blank=True, default=None, on_delete=models.SET_NULL)
+    
+    def __str__(self):
+        return f"{self.name} "
 
 # Create your models here.
 # PRODUCT is sql table
@@ -20,18 +31,11 @@ class Product(models.Model):
     variant = models.BooleanField()
     active = models.BooleanField()
     discount = models.BooleanField()
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    category = models.ForeignKey(Categories, null=True, blank=True, default=None, on_delete=models.SET_NULL)
     def __str__(self):
         return f"{self.name} "
 
-class Categories(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    child_node = models.ForeignKey(Categories)
-    right_node = models.ForeignKey(Categories)
-    
-    def __str__(self):
-        return f"{self.name} "
+
 
 
 
