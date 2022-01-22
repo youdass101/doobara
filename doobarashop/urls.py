2d1f313cd2ab5c854642c718fa06987cf7f3d0be
Aech <hamzechalhoub@gmail.com>
2022-01-22 19:47:59 +0200

    s

--- doobarashop/urls.py
+++ doobarashop/urls.py
@@ -7,19 +7,31 @@ from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns=[
+    # Index page or Home page 
     path("", views.index, name="index"),
+    # Shop page for all products 
     path("shop", views.shop, name="shop"),
-    path("blog", views.blog, name="blog"),
-    path("video", views.video, name="video"),
-    path("contactus", views.contactus, name="contactus"),
-    path("myaccount", views.myaccount, name="myaccount"),
-    path("cart", views.cart, name="cart"),
-    path('single_product/<str:locat>/', views.single_product, name="single_product"),
-    path("single_blog_post", views.single_blog_post, name="single_blog_post"),
-    path("checkout", views.checkout, name="checkout"),
-    path("register_login", views.register_login, name="register_login"),
+    # Shop page filtering product subpage
     path("shop/<str:locat>/", views.filtering, name="filtering"),
-    # all auth
+    # Blog carousel page
+    path("blog", views.blog, name="blog"),
+    # Video Carousel page
+    path("video", views.video, name="video"),
+    # Contact Us page 
+    path("contactus", views.contactus, name="contactus"),
+    # My Account page
+    path("myaccount", views.myaccount, name="myaccount"),
+    # Cart page 
+    path("cart", views.cart, name="cart"),
+    # Single product page
+    path('single_product/<str:locat>/', views.single_product, name="single_product"),
+    # Single Blog Post page
+    path("single_blog_post", views.single_blog_post, name="single_blog_post"),
+    # Chekout cart page 
+    path("checkout", views.checkout, name="checkout"),
+    # Login and registration page 
+    path("register_login", views.register_login, name="register_login"),
+    # all auth views
     path('accounts/', include('allauth.urls')),
     path('logout', LogoutView.as_view()),
 
