3a8facb7212fc4c99a364535dd67c83ae5cabd9f
youdass101 <hamzechalhoub@gmail.com>
2022-01-27 02:47:20 +0200

    templates orgiizing account app duplicate error

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog(request):
-    return render(request, "doobarashop/blog.html")
+    return render(request, "blog/blog.html")
 
 def video(request):
-    return render(request, "doobarashop/video.html")
+    return render(request, "blog/video.html")
 
 def single_blog_post(request):
-    return render(request, "doobarashop/single_blog_post.html")
+    return render(request, "blog/single_blog_post.html")
--- doobara/settings.py
+++ doobara/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'shop',
     'cart',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
