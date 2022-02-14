9318b5973cde90c4c7d4e5cf816d9f1edd478606
youdass101 <hamzechalhoub@gmail.com>
2022-02-15 01:49:30 +0200

    phone cart

--- doobara/settings.py
+++ doobara/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '&&#g)kp!ad^4*gyx6x-d&(tku-xk5!d9@8v%vvnl1wrqh+s$#='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
