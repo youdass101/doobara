e33260f5d328d7ed146b7626aecc0cbf16fb815c
youdass101 <hamzechalhoub@gmail.com>
2026-03-08 01:27:47 +0200

    finished card and hero design

--- doobara/settings.py
+++ doobara/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'livereload.middleware.LiveReloadScript',
