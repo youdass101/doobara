84e9063eb08d13ccaa1f742afb29bc3d493b81f6
youdass101 <hamzechalhoub@gmail.com>
2026-03-30 22:49:51 +0300

    requirements

--- doobara/urls.py
+++ doobara/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
         TemplateView.as_view(template_name='robots.txt', content_type='text/plain'),
         name='robots_txt',
     ),
-    path('admin/', admin.site.urls),
+    path('hamzeadmin/', admin.site.urls),
     path("", include("shop.urls")),
     path("", include("cart.urls")),
     path("", include("users.urls")),
