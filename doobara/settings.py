303d48f7d976f350704efc470f685176186bf625
youdass101 <hamzechalhoub@gmail.com>
2026-03-31 17:59:08 +0300

    production settings

--- doobara/settings.py
+++ doobara/settings.py
@@ -283,8 +283,6 @@ X_FRAME_OPTIONS = "DENY"
 CSRF_TRUSTED_ORIGINS = env.list(
     "CSRF_TRUSTED_ORIGINS",
     default=[
-        "http://127.0.0.1:8000",
-        "http://localhost:8000",
         "https://doobara.com",
         "https://www.doobara.com",
     ],
