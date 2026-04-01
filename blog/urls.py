65a56991812bf1b89fff82fb4a492898ed29b1c6
youdass101 <hamzechalhoub@gmail.com>
2026-04-01 17:50:52 +0300

    policy

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,15 @@ from . import views
 from django.urls import path
 
 
-urlpatterns=[   
+urlpatterns = [
     # Blog carousel page
     path("blog", views.blog, name="blog"),
     # Video Carousel page
     path("video", views.video, name="video"),
     # Single Blog Post page
     path("single_blog_post", views.single_blog_post, name="single_blog_post"),
+    # Informational pages
+    path("privacy-policy", views.privacy_policy, name="privacy_policy"),
+    path("shipping-policy", views.shipping_policy, name="shipping_policy"),
+    path("user-deletion-instruction", views.user_deletion_instruction, name="user_deletion_instruction"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from .models import *
 
+
 # Create your views here.
 def blog(request):
     return render(request, "blog/blog.html")
+
 
 def video(request):
     video = Video.objects.all()
     video = [v.serialize() for v in video]
 
-    return render(request, "blog/video.html",{"video": video})
+    return render(request, "blog/video.html", {"video": video})
+
 
 def single_blog_post(request):
     return render(request, "blog/single_blog_post.html")
+
+
+def privacy_policy(request):
+    return render(request, "blog/privacy_policy.html")
+
+
+def shipping_policy(request):
+    return render(request, "blog/shipping_policy.html")
+
+
+def user_deletion_instruction(request):
+    return render(request, "blog/user_deletion_instruction.html")
